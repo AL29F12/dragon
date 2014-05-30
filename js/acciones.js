b03fca80a2c56dd8fa91ad6448f7c7a0b2374d5e
kevin quintero reyes <yeykc6@gmail.com>
2014-05-30 11:00:49 -0500

    new
    newww

--- js/acciones.js
+++ js/acciones.js
@@ -1,8 +1,13 @@
 // JavaScript Document
- $('#B_Vibrar').tap(function(){
+ $(document).ready(function(e) {
+ document.addEventListener("deviceready",function(){
+  $('#B_Vibrar').tap(function(){
     navigator.notification.vibrate(500);
 	});
 	
 	$('#B_beep').tap(function(){
 	 navigator.notification.beep(1);
    });
+   
+ },false); //deviceready
+}); //document ready
