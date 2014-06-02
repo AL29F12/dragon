18a8e7336f2a77862fd41c4a5cf6c0990200c5bd
kevin quintero reyes <yeykc6@gmail.com>
2014-06-02 14:27:41 -0500

    dfvgbn
    dfghnjm

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,5 @@
 // JavaScript Document
- $(document).ready(function(e) {
- document.addEventListener("deviceready",function(){
+
   $('.button').tap(function(){
     navigator.notification.vibrate(500);
 	});
@@ -8,6 +7,3 @@
 	$('.button2').tap(function(){
 	 navigator.notification.beep(1);
    });
-   
- },false); //deviceready
-}); //document ready
